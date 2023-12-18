8e67d96ece0045d57a4ad67ed952168ae4480a92
Osvy1994 <osvaldo.elvirez@gmail.com>
2023-12-17 20:39:38 -0500

    removed config.js from .gitignore

---
+++ config.js
@@ -0,0 +1,7 @@
+export const config = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "28509f430emsh2581ea19cf6cc2ap1ac7f0jsndb28dcae8ae4",
+    "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+  },
+};
